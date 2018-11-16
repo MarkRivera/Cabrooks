fc816975a36a6876857ed081bbc13bc7eda4b9c8
Mark Rivera <Mrivera1991@gmail.com>
2018-11-16 18:32:22 -0500

    implement features, such as responsive gallery and navigation. Also hero-image and animations

--- scripts/index.js
+++ scripts/index.js
@@ -1,18 +1,48 @@
 document.addEventListener("DOMContentLoaded", function (event) {
   Barba.Pjax.start();
 
-  var HideShowTransition = Barba.BaseTransition.extend({
-    start: function() {
-      this.newContainerLoading.then(this.finish.bind(this));
-    },
+  /*Check if landing page was loaded first,
+  if so, then use the barba event listener.
+  Otherwise, use native event listener.
+  */
 
-    finish: function() {
-      document.body.scrollTop = 0;
-      this.done();
-    }
-  });
+  if (window.location.pathname === '/') {
+    Barba.Dispatcher.on('newPageReady', function () {
+      let nav = document.getElementById("myTopnav");
+      nav.addEventListener('click', function () {
+        (nav.className === "topnav topnav-1a") ? nav.className += " responsive": nav.className = "topnav topnav-1a";
+      })
+      initSlideShow();
+    })
+  }
+  else {
+    let nav = document.getElementById("myTopnav");
+    nav.addEventListener('click', function () {
+      (nav.className === "topnav topnav-1a") ? nav.className += " responsive": nav.className = "topnav topnav-1a";
+    })
+    initSlideShow ()
+  }
 
-  Barba.Pjax.getTransition = function() {
-    return HideShowTransition;
+  /* Slide show JS */
+
+  function initSlideShow () {
+    const el = document.querySelector('.siema');
+    const slideShow = new Siema({
+      selector: el,
+      duration: 200,
+      easing: 'ease-out',
+      perPage: 1,
+      startIndex: 0,
+      draggable: true,
+      multipleDrag: true,
+      threshold: 20,
+      loop: true,
+      rtl: false,
+      onInit: () => {},
+      onChange: () => {},
+    });
+    const prev = document.querySelector('.prev').addEventListener('click', () => slideShow.prev())
+    const next = document.querySelector('.next').addEventListener('click', () => slideShow.next())
   };
+
 })
